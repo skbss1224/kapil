ca593180e656dba6ab5042ac4b2c949f62c12b63
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-14 19:20:02 +0530

    data_types
    java

---
+++ data_types.java
@@ -0,0 +1,28 @@
+package kabil;
+
+public class data_types {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//data types
+		byte a=-128;
+		System.out.println(a);
+		short b=32767;
+		System.out.println(b);
+		int c=2147483647;
+		System.out.println(c);
+		long d=9223372036854775807l;
+		System.out.println(d);
+		float e=677.8987876f;
+		System.out.println(e);
+		double f=8733.938838d;
+		System.out.println(f);
+		boolean g=false;
+		System.out.println(g);
+		char i='s';
+		System.out.println(i);
+		String j="23456789sdfghjk!@#$%^&%^&*";
+		System.out.println(j);
+	}
+
+}
