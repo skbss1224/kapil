97d1ce19e665a11e1dd1163b2cb8212102f52e60
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-07-14 19:45:49 +0530

    arithmetic
    java

---
+++ operator.java
@@ -0,0 +1,30 @@
+package kabil;
+
+public class operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//operators
+		//arithmetic operator
+		// + plus operator
+		// - minus operator
+		// * mul operator
+		// / div operator
+		// % per operator
+		int kapil=10000;
+		int sasi=70000;
+		int total;
+		total=sasi+kapil;
+		
+		System.out.println("plus operator:"+total);
+		total=sasi-kapil;
+		System.out.println("minus operator:"+total);
+		total=sasi*kapil;
+		System.out.println("mul operator:"+total);
+		total=sasi/kapil;
+		System.out.println("div operator:"+total);
+		total=sasi%kapil;
+		System.out.println("per operator"+total);
+	}
+
+}
