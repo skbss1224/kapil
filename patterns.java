93d7d9627de615f09535e519c69a6f61bdb6f695
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-08-05 16:04:47 +0530

    pyramid,right,left
    java

--- patterns.java
+++ patterns.java
@@ -4,10 +4,52 @@ public class patterns {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int rows=5;
-		for(int i=1;i<=rows;++i) {
-			for(int j=1;j<=i;++j) {
-				System.out.print(j+" ");
+//		int rows=5;
+//		for(int i=1;i<=rows;++i) {
+//			for(int j=1;j<=i;++j) {
+//				System.out.print(j+" ");
+//			}
+//			System.out.println();
+//		}
+		//square star
+//		int n=5;
+//		for (int i=0;i<n;i++) {
+//			for(int j=0;j<n;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		//right upper triangle
+//		int size=5;
+//		for (int i=0;i<size;i++) {
+//			for(int j=1;j<size-i;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=0;k<=i;k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		//right down
+//		int size =5;
+//		for(int i=0;i<size;i++) {
+//			for(int j=0;j<i;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j=size;j>i;j--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		//pyramid star pattern
+		int size=5;
+		for(int i=0;i<size;i++) {
+			for(int j=0;j<size-i-1;j++) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<2*i+1;k++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
